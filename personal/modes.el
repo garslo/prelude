8a99720c2699e21d699af9d3aa66886d9ffdb5fa
Gary Slopsema <gslopsema@llnw.com>
2014-03-13 21:34:55 -0400

    Fixed load error

--- personal/modes.el
+++ personal/modes.el
@@ -10,3 +10,19 @@
 (add-hook 'prog-mode-hook (lambda ()
                             (local-set-key (kbd "RET")
                                            'electrify-return-if-match)))
+                                        ; -- paredit enhancement from EmacsWiki -- ;
+(defvar electrify-return-match
+  "[\]}\)\"]"
+  "If this regexp matches the text after the cursor, do an \"electric\"
+return.")
+
+(defun electrify-return-if-match (arg)
+  "If the text after the cursor matches `electrify-return-match' then
+open and indent an empty line between the cursor and the text. Move the
+cursor to the new line."
+  (interactive "P")
+  (let ((case-fold-search nil))
+    (if (looking-at electrify-return-match)
+        (save-excursion (newline-and-indent)))
+    (newline arg)
+    (indent-according-to-mode)))
